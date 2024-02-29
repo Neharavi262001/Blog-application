8dbec7acd95d8dcc44266e8d32a0aa796916f7c4
Neharavi262001 <neharavi262001@gmail.com>
2024-02-29 23:14:09 +0530

    .

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,10 +17,7 @@ passwordSchema
 export const register = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body;
     
-     if (!name || !email || !password){
-        res.status(400)
-        throw new Error('All fields must be filled')  
-     }
+    
 
     // Strong password validation
     if (!passwordSchema.validate(password)) {
@@ -35,6 +32,11 @@ export const register = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Email already in use')
     }
+
+    if (!name || !email || !password){
+        res.status(400)
+        throw new Error('All fields must be filled')  
+     }
 
     // Create user
     const user = await User.create({ name, email, password });
