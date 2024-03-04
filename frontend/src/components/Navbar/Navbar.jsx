bb14d86aa0101d2873cb33015d494e94c63ff02a
Neharavi262001 <neharavi262001@gmail.com>
2024-03-04 09:43:53 +0530

    .

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import'./navbar.css'
 import { Link, useNavigate } from 'react-router-dom'
-import { FaBlog, FaBlogger, FaHome, FaPlus, FaPlusCircle, FaUser } from 'react-icons/fa'
+import { FaBars, FaBlog, FaBlogger, FaHome, FaPlus, FaPlusCircle, FaTimes, FaUser } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
 
 const Navbar = () => {
   const navigate = useNavigate()
   const {userDetails}=useSelector((state)=>state.auth)
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
   return (
-    <div className='navbar'>
+    <div className={`navbar ${mobileMenuOpen ? 'mobile-menu-open' : ''}`}>
         <Link to='/'><h2><FaBlogger/> Bloggle</h2></Link>
-        <div className="nav-links">
+        <div className={`nav-links ${mobileMenuOpen ? 'mobile-menu' : ''}`}>
 
             <Link to="/" className="nav-item"><FaHome/> Home </Link>
             {userDetails && <Link to='/profile' className='nav-item'> <FaUser/> Profile</Link>}
             {userDetails ? <button  onClick={()=>navigate('/post')}><FaPlus/> Post</button> :   <Link to="/login" className="nav-item">Login</Link>}
+
           
-           
         </div>
+        <div className='mobile-menu-btn' onClick={toggleMobileMenu}>
+              {mobileMenuOpen ? <FaTimes /> : <FaBars />}
+            </div> 
+
+        
     </div>
   )
 }
