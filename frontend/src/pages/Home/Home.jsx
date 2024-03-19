036ea78d046f158a911bb6f5e178793c858a75db
Neharavi262001 <neharavi262001@gmail.com>
2024-03-19 19:33:48 +0530

    created a landing page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import './home.css'
 import PostList from '../../components/PostList/PostList'
+import bg from '../../images/bg.png'
 
 const Home = () => {
   return (
     <div className='home'>
+      <div className="landing-page">
+      <div className="welcome-message">
+          <h1>Welcome to Bloggle.</h1>
+          <p>Your go-to platform for sharing and discovering amazing blog posts!</p>
+          <button> <a href="#all-posts">All Posts</a></button>
+        </div>
+        <img src={bg} alt="" />
+        
+       
+      </div>
+      <div id="all-posts">
+        <h1>All Posts</h1>
       <PostList/>
+      </div>
+      
     </div>
   )
 }
